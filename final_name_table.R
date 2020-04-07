a61635c410dcd8de029cf63b86d37248e0a990d4
Mira Sohn <tonton07@gmail.com>
2020-04-07 00:05:37 -0400

    20 popular baby names in 2011-2015 in England/Wales vs US

--- final_name_table.R
+++ final_name_table.R
@@ -1,37 +1,33 @@
 library(tidyverse)
-library(ggplot2)
 library(formattable)
-eboy <- read_csv('e_boy.csv')
+eboy <- read_csv('e_boys.csv')
 egirl <- read_csv('e_girls.csv')
-nboy <- read_csv('ny_boys.csv')
-ngirl <- read_csv('ny_girls.csv')
+uboy <- read_csv('us_boys.csv')
+ugirl <- read_csv('us_girls.csv')
 
-# data frames for Top 10 popular names of boys and girls in England Wales vs NYC in 2011-2015
-boy_names <- data.frame(England_Wales_Boy = eboy[1:10, 'Name'], 
-                        England_Wales_Boy = eboy[1:10, 'Count'],
-                        NYC_Boy = nboy[1:10, 'Name'], 
-                        NYC_Boy = nboy[1:10, 'Counts'])
 
-girl_names <- data.frame(England_Wales_Girl = egirl[1:10, 'Name'],
-                         England_Wales_Girl = egirl[1:10, 'Count'],
-                         NYC_Girl = ngirl[1:10, 'Name'],
-                         NYC_Girl = ngirl[1:10, 'Counts'])
+boy_names <- data.frame(England_Wales_Boy = eboy[1:20, 'Name'], 
+                        England_Wales_Boy = eboy[1:20, 'Count'],
+                        US_Boy = uboy[1:20, 'Name'], 
+                        US_Boy = uboy[1:20, 'Counts'])
 
-names(boy_names) <- c('England Wales Boy Names',
-                      'England Wales Boy Numbers',
-                      'NYC Boy Names',
-                      'NYC Boy Numbers')
+girl_names <- data.frame(England_Wales_Girl = egirl[1:20, 'Name'],
+                         England_Wales_Girl = egirl[1:20, 'Count'],
+                         US_Girl = ugirl[1:20, 'Name'],
+                         US_Girl = ugirl[1:20, 'Counts'])
+name <- c('Names in England/Wales', 
+          'Numbers in England/Wales',
+          'Names in the United States', 
+          'Numbers in the United States')
 
-names(girl_names) <- c('England Wales Girl Names',
-                      'England Wales Girl Numbers',
-                      'NYC Girl Names',
-                      'NYC Girl Numbers')
+names(boy_names) <- name
+names(girl_names) <- name
 
 # tables for boys and girls
 table_boys <- formattable(boy_names, 
-            list(area(col = c('England Wales Boy Numbers','NYC Boy Numbers')) ~ normalize_bar("gold", 0.2)))
+            list(area(col = c('Numbers in England/Wales','Numbers in the United States')) ~ normalize_bar("gold", 0.2)))
 
 table_girls <- formattable(girl_names, 
-                           list(area(col = c('England Wales Girl Numbers','NYC Girl Numbers')) ~ normalize_bar("greenyellow", 0.2)))
+                           list(area(col = c('Numbers in England/Wales','Numbers in the United States')) ~ normalize_bar("greenyellow", 0.2)))
 
 
