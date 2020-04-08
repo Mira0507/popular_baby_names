010b9ef0f3aa65d7551b43c4da94654968b9e4b9
Mira Sohn <tonton07@gmail.com>
2020-04-07 21:38:41 -0400

    final data presetation of 20 most popular boy/girl names in England Wales and the US

--- final_name_table.R
+++ final_name_table.R
@@ -30,4 +30,26 @@ table_boys <- formattable(boy_names,
 table_girls <- formattable(girl_names, 
                            list(area(col = c('Numbers in England/Wales','Numbers in the United States')) ~ normalize_bar("greenyellow", 0.2)))
 
+# ebn20 = 20 popular england boy names
+# ubn20 = 20 popular us boy names
+ebn20 <- boy_names[[1]]
+ubn20 <- boy_names[[3]]
 
+# egn20 = 20 popular england girl names
+# ugn20 = 20 popular us girl names
+egn20 <- girl_names[[1]]
+ugn20 <- girl_names[[3]]
+
+# unique_ebn20 = england boy names not popular in the us
+# unique_ubn20 = us boy names not popular in england
+# common_bn20 = common boy names
+unique_ebn20 <- ebn20[!ebn20 %in% ubn20]
+unique_ubn20 <- ubn20[!ubn20 %in% ebn20]
+common_bn20 <- ebn20[ebn20 %in% ubn20]
+
+# unique_egn20 = england girl names not popular in the us
+# unique_ugn20 = us girl names not popular in england
+# common_gn20 = common girl names
+unique_egn20 <- egn20[!egn20 %in% ugn20]
+unique_ugn20 <- ugn20[!ugn20 %in% egn20]
+common_gn20 <- egn20[egn20 %in% ugn20]
